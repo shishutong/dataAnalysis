c84e15641cb9908d831fea45e46bdad0fd2ec559
shishutong <shishutong49@gmail.com>
2019-06-12 23:17:37 +0800

    2019.6.12

--- CreateArrageRange.py
+++ CreateArrageRange.py
@@ -57,4 +57,26 @@ print(a)
 
 a = np.linspace(1, 10, 10, retstep=True)
 print(a)
-b =
+b = np.linspace(1, 10, 10).reshape([10, 1])
+print(b)
+
+"""
+numpy.logspace
+numpy.logspace 函数用于创建一个于等比数列。格式如下：
+
+np.logspace(start, stop, num=50, endpoint=True, base=10.0, dtype=None)
+base 参数意思是取对数的时候 log 的下标。
+
+参数	描述
+start	序列的起始值为：base ** start
+stop	序列的终止值为：base ** stop。如果endpoint为true，该值包含于数列中
+num	要生成的等步长的样本数量，默认为50
+endpoint	该值为 ture 时，数列中中包含stop值，反之不包含，默认是True。
+base	对数 log 的底数。
+dtype	ndarray 的数据类型
+"""
+a = np.logspace(1.0, 2.0, num=10)
+print(a)
+
+b = np.logspace(0, 9, 10, base=2)
+print(b)
