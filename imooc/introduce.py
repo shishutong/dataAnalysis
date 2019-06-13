1be8e3bcce1ad02b9cd89cf0efcf5f00a7030e42
shishutong <shishutong49@gmail.com>
2019-06-13 23:25:40 +0800

    慕课网数据分析

---
+++ imooc/introduce.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time  : 2019/6/13 23:24
+# @Author: Gao Chengdong
+# @Project: dataAnalysis
+# @File  : introduce.py
+
+"""
+1.Numpy 数据结构基础
+2.scipy 强大的科学计算方法
+3.matplotlib 丰富的可视化套件
+4.pandas 基础数据分析套件
+5.scilit-learn 数据分析建模库
+6.keras 人工神经网络
+"""
